33271b50821fea63ad9280b9cc2142c65bd60eb6
celilcecen <60426786+celilcecen@users.noreply.github.com>
2023-08-09 11:55:03 +0300

    Code Generator

---
+++ BillScanner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Tesseract;
+
+namespace BillScanner
+{
+    class BillScanner
+    {
+        static void Main(string[] args)
+        {
+            // Örnek JSON yanıtı
+            string jsonResponse = @"{
+                ""description"": ""FIRMA ADI: Örnek Market\nTARİH: 2023-07-15\nTOPLAM TUTAR: 150.00 TL"",
+                ""coordinates"": {
+                    ""x"": 100,
+                    ""y"": 50,
+                    ""width"": 300,
+                    ""height"": 100
+                }
+            }";
+
+           
+            JObject parsedResponse = JObject.Parse(jsonResponse);
+
+            // Reading Text and Coordinates
+            string description = parsedResponse["description"].ToString();
+            JObject coordinates = parsedResponse["coordinates"] as JObject;
+
+            int x = (int)coordinates["x"];
+            int y = (int)coordinates["y"];
+            int width = (int)coordinates["width"];
+            int height = (int)coordinates["height"];
+
+            string imagePath = "path_to_your_image.jpg";
+
+            //  OCR Engine
+            using (var engine = new TesseractEngine("./tessdata", "eng", EngineMode.Default))
+            {
+                // Taking Image
+                using (var img = Pix.LoadFromFile(imagePath))
+                {
+                    // Scanning Coordinate
+                    using (var page = engine.Process(img, Rect.FromCoords(x, y, x + width, y + height)))
+                    {
+                        string text = page.GetText();
+
+                        
+                        Console.WriteLine("Scanned Bill Context : ");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CodeGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace UniqueCodeGenerator
+{
+    class CodeGenerator
+    {
+        static HashSet<string> existingCodes = new HashSet<string>();
+        static int maxAttempts = 1000;
+        static int codeLength = 8;
+        static string characters = "ACDEFGHKLMNPRTXYZ234579";
+
+        static void Main(string[] args)
+        {
+            GenerateUniqueCode();
+        }
+
+        static void GenerateUniqueCode()
+        {
+            Random random = new Random();
+            StringBuilder codeBuilder = new StringBuilder();
+
+            for (int attempt = 0; attempt < maxAttempts; attempt++)
+            {
+                codeBuilder.Clear();
+                for (int i = 0; i < codeLength; i++)
+                {
+                    int randomIndex = random.Next(0, characters.Length);
+                    char randomChar = characters[randomIndex];
+                    codeBuilder.Append(randomChar);
+                }
+
+                string uniqueCode = codeBuilder.ToString();
+
+                if (!existingCodes.Contains(uniqueCode))
+                {
+                    existingCodes.Add(uniqueCode);
+                    Console.WriteLine("Generated Unique Code: " + uniqueCode);
+                    break;
+                }
+
+                if (attempt == maxAttempts - 1)
+                {
+                    Console.WriteLine("Maximum attempts reached. Could not generate a unique code.");
+                }
+            }
+        }
+    }
+}
